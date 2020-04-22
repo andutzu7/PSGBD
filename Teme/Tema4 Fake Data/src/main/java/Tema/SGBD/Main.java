e6971741a74480cb90b417d41f405b5143c58eb9
andutzu7 <rockkidd57@gmail.com>
2020-04-22 11:00:18 +0300

    tema 4 init

---
+++ Teme/Tema4 Fake Data/src/main/java/Tema/SGBD/Main.java
@@ -0,0 +1,22 @@
+package Tema.SGBD;
+
+import java.sql.*;
+
+class Main {
+    public static void main(String args[]) {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:xe", "STUDENT", "STUDENT");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from STUDENTI");
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+            con.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
