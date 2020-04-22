a8a805373de6a338b2284acff395c71bee39f7d9
andutzu7 <rockkidd57@gmail.com>
2020-04-22 17:35:05 +0300

    created the leaderboard 1.0, gotta change that

--- Teme/Tema3 ListaPrieteni/src/Tema/SGBD/Main.java
+++ Teme/Tema3 ListaPrieteni/src/Tema/SGBD/Main.java
@@ -1,9 +1,7 @@
 package Tema.SGBD;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 class Main {
     private static Connection connection = null;
@@ -41,7 +39,7 @@ class Main {
         String insertQuery = "INSERT INTO Prieteni values(?,?,?,?,?)";
         String getDataQuery = "SELECT nume,prenume FROM Studenti WHERE id = ?";
         int indexBound = 50;
-        int id=1;
+        int id = 1;
         PreparedStatement insertStatement = connection.prepareStatement(insertQuery);
 
         PreparedStatement getDataStatement = connection.prepareStatement(getDataQuery);
@@ -62,7 +60,7 @@ class Main {
                 String prenume = rs.getString(2);
                 insertStatement.setInt(1, id++);
                 insertStatement.setInt(2, i);
-                insertStatement.setInt(3,generatedIndex);
+                insertStatement.setInt(3, generatedIndex);
                 insertStatement.setString(4, nume);
                 insertStatement.setString(5, prenume);
                 insertStatement.executeQuery();
@@ -70,12 +68,58 @@ class Main {
         }
     }
 
+    public static void printMap(Map<Integer, Integer> board) {
+        Set set = board.entrySet();//Converting to Set so that we can traverse
+        Iterator itr = set.iterator();
+        while (itr.hasNext()) {
+            //Converting to Map.Entry so that we can get key and value separately
+            Map.Entry entry = (Map.Entry) itr.next();
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    /*
+     * Ideea functiei: selectam toti prietenii pe care personID ii are. Apoi selectam prietenii fiecaruia dintre
+     * acestia si ii adaugam intr un map pe care il adaugam intr-o lista, creeind un clasament
+     *  La final jsonificam rezultatul si il printam in consola.
+     * */
+    public static void suggestFriends(int personId) throws SQLException {
+        String getFriendsQuery = "SELECT id_prieten FROM Prieteni WHERE id_pers = ?";
+        PreparedStatement getDataStatement = connection.prepareStatement(getFriendsQuery);
+        Map<Integer, Integer> board = new HashMap<Integer, Integer>();//id_prieten,nr aparitii
+        List<Integer> userFriendsIds = new ArrayList<>();
+        getDataStatement.setInt(1, personId);
+        ResultSet rs = getDataStatement.executeQuery();
+        //Stocam indexurile fiecarui prieten al userului specificat
+        while (rs.next()) {
+            userFriendsIds.add(rs.getInt(1));
+            board.put(rs.getInt(1), 1);
+        }
+
+        for (int i = 0; i < userFriendsIds.size(); i++) {
+            getDataStatement.setInt(1, userFriendsIds.get(i));
+            rs = getDataStatement.executeQuery();
+            while (rs.next()) {
+                int value;
+                if (board.containsKey(rs.getInt(1))) {
+                    value = board.get(rs.getInt(1));
+                } else {
+                    value = 0;
+                }
+                board.put(rs.getInt(1), value + 1);
+
+            }
+        }
+        printMap(board);
+    }
+
     public static void main(String[] args) throws SQLException {
         Database db = Database.getInstance();
         Main m = new Main();
-       //createTablePrieteni();
+        //createTablePrieteni();
         //Pentru 10 studenti creem o lista de prieteni cu 20 de persoana
         //populateFriendsTable(10,20);
+        suggestFriends(1);
         connection.close();
     }
 }
