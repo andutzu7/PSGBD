7c61568c9d8ea2501b1e7d97627d805ddf499b1a
andutzu7 <rockkidd57@gmail.com>
2020-04-27 17:26:29 +0300

    made the table to xml script

---
+++ Teme/Tema7 XML Export/src/main/java/Tema/SGBD/Main.java
@@ -0,0 +1,14 @@
+package Tema.SGBD;
+import javax.xml.parsers.*;
+import org.xml.sax.*;
+import org.xml.sax.helpers.*;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
