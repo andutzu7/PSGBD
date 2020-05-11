0848001a87356877bd1dd52758d84d2a9519b0a3
andutzu7 <rockkidd57@gmail.com>
2020-05-12 02:05:41 +0300

    almost finished the createlist function

---
+++ Teme/BIG PROBLEM/src/Tema/SGBD/Main.java
@@ -0,0 +1,37 @@
+package Tema.SGBD;
+
+import java.sql.*;
+
+class Main {
+    private static Connection connection = null;
+
+    Main() {
+        try {
+            connection = Database.getInstance().getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void executePLSQLFunction() throws SQLException {
+        CallableStatement cstmt = connection.prepareCall("{?= call urmatoarea_intrebare(?,?)}");
+        String email="ion@yahoo.com";
+        String rezolvare="";
+        cstmt.registerOutParameter(1, Types.VARCHAR);
+        cstmt.setString(2, email);
+        cstmt.setString(3, rezolvare);
+        cstmt.execute();
+        String result= cstmt.getString(1);
+        System.out.print(result);
+
+    }
+    public static void main(String[] args) {
+        try {
+            Database db = Database.getInstance();
+            Main m = new Main();
+            executePLSQLFunction();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
